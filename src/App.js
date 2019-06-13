a6b8ba1e4a7567f1042daab787cf21c18ed1b035
Iqbal Fasri <iqbalx64@gmail.com>
2019-06-13 12:12:10 +0700

    Function created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const myStyle = {
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  color: "white"
+};
 
 function App() {
+  const [amount, setAmount] = useState("");
+
+  const formating = amount => {
+    // Handle carachter
+    let toRp = amount.replace(/[^,\d]/g, "").toString();
+    let split = toRp.split(",");
+    //
+    let sisa = split[0].length % 3;
+    //
+    let rupiah = split[0].substr(0, sisa);
+    //
+    let ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+    if (ribuan) {
+      let separator = sisa ? "." : "";
+      let _rp = rupiah += separator + ribuan.join(".");
+      setAmount(_rp)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={myStyle}>
+      <h1>Mata uang indonesia</h1>
+      {amount}
+      <input onChange={e => formating(e.target.value)} />
     </div>
   );
 }
